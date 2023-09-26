7fd0314f9d51d48b9a692b7cf18d479b8e093af3
Pedro Lages Ribeiro <plagesribeiro@gmail.com>
2023-09-26 19:59:19 -0300

    new profile

--- src/lib/site-config.js
+++ src/lib/site-config.js
@@ -7,7 +7,7 @@ export const website = `https://pedrolagesribeiro.com.br`
 export const description = `Aqui é onde eu escrevo sobre o que eu aprendo, o que eu penso e o que eu faço... Quando dá!`
 
 export const initials = `PLR`
-export const photoUrl = `/assets/profile.png`
+export const photoUrl = `https://media.licdn.com/dms/image/D4D03AQFAZU-w-pqhLQ/profile-displayphoto-shrink_800_800/0/1694291274196?e=1701302400&v=beta&t=IJHuz6_y88YTL4eh7iNF9d4FVsQ0CW3A15-92Xpb7v4`
 
 export const pages = [
   { title: `Principal`, path: `/` },
